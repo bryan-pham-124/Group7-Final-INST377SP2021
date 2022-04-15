ed108adbb5d76dc4de880a21a77a7f63abd73b8d
bryan-pham-124 <72675857+bryan-pham-124@users.noreply.github.com>
2022-04-14 20:53:14 -0400

    Revert "Finished Get Controlleds for /api/food endpoint"

--- server/database/config.js
+++ server/database/config.js
@@ -3,35 +3,28 @@
    even for your group project, because it will stop your DB from connecting
    due to the many loops in the initializeDB file
    ~ Prof A.
-
-       username: 'root',
-    password: 'Veracrypt@12',
-    database: 'Dining_Hall_Tracker',
-    host: 'localhost',
-    dialect: 'mysql'
 */
 
 export default {
   development: {
-    username: 'ubuntu',
-    password: 'Veracrypt@12!',
-    database: 'Group7_DHT',
+    username: 'student',
+    password: 'INST377@UMD',
+    database: 'Dining_Hall_Tracker',
     host: '174.129.198.86',
-    dialect: 'mysql',
+    dialect: 'mysql'
   },
-
   test: {
-    username: 'ubuntu',
-    password: 'Veracrypt@12!',
-    database: 'Group7_DHT',
+    username: 'student',
+    password: 'INST377@UMD',
+    database: 'Dining_Hall_Tracker',
     host: '174.129.198.86',
-    dialect: 'mysql',
+    dialect: 'mysql'
   },
   production: {
-    username: 'ubuntu',
-    password: 'Veracrypt@12!',
-    database: 'Group7_DHT',
+    username: 'student',
+    password: 'INST377@UMD',
+    database: 'Dining_Hall_Tracker',
     host: '174.129.198.86',
-    dialect: 'mysql',
+    dialect: 'mysql'
   }
 };
